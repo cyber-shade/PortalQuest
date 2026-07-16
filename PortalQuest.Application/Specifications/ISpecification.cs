0955de9f2cf97460fcac6764355c7e46145a039c
Ali Gharedaghi <aligr.work@gmail.com>
2026-07-16 17:10:56 +0330

    specification added

---
+++ PortalQuest.Application/Specifications/ISpecification.cs
@@ -0,0 +1,18 @@
+﻿using System.Linq.Expressions;
+
+namespace PortalQuest.Application.Specifications
+{
+	public interface ISpecification<T>
+	{
+		Expression<Func<T, bool>>? Criteria { get; }
+		List<Expression<Func<T, object>>> Includes { get; }
+		List<string> IncludeStrings { get; }
+		Expression<Func<T, object>>? OrderBy { get; }
+		Expression<Func<T, object>>? OrderByDescending { get; }
+		int Skip { get; }
+		int Take { get; }
+		bool IsPagingEnabled { get; }
+		bool AsNoTracking { get; }
+		bool AsSplitQuery { get; }
+	}
+}
